d278f353d8786f655979dd22f652d77ceb1ee015
kimsong <saokimsong@gmail.com>
2023-09-11 13:36:07 +0700

    Initial

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
